74bdd20e12f78bcfff87a81ca141506808d52df3
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-07-13 12:03:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  // Trim any leading or trailing whitespace
+  str = str.trim();
+
+  // Find the index of the first space
+  const spaceIndex = str.indexOf(' ');
+
+  // If no space is found, return the entire string
+  if (spaceIndex === -1) {
+    return str;
+  }
+
+  // Otherwise, return the substring from the start to the first space
+  return str.substring(0, spaceIndex);
 }
 
 // Do not change the code below
